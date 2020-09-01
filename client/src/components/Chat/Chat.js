5ea6a19a7c50f4546a399b6cc602989d549de701
ValeedAnjum <allinone644@gmail.com>
2020-09-01 09:09:15 +0500

    some restructring and sending message from client to server using socket.emit

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+
+let socket;
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const ENDPOINT = "http://localhost:5000";
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    socket = io(ENDPOINT);
+    setName(name);
+    setRoom(room);
+    socket.emit("join", { name, room }, () => {});
+  }, [ENDPOINT, location.search]);
+  return <div>Chat</div>;
+};
+
+export default Chat;
--- server/index.js
+++ server/index.js
@@ -2,10 +2,24 @@ const express = require("express");
 const app = express();
 const socketio = require("socket.io");
 const http = require("http");
-
+const router = require("./router");
 const PORT = process.env.PORT || 5000;
 
+//creating a  http server;
 const server = http.createServer(app);
+//socket.io instance
 const io = socketio(server);
 
+//on new connection and disconnection
+io.on("connection", (socket) => {
+  socket.on("join", ({ name, room }, callback) => {
+    console.log(name, room);
+  });
+
+  io.on("disconnect", () => {
+    console.log("User had left");
+  });
+});
+
+app.use(router);
 server.listen(PORT, () => console.log(`Server is running at PORT ${PORT} `));
